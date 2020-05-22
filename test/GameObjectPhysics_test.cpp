32d6088d4125fce765d8163c369c9645ee4c7fb7
sandro4912 <sandro.wissmann@gmail.com>
2020-05-22 12:42:32 +0200

    test: adds more tests for GameObjectPhysics

--- test/GameObjectPhysics_test.cpp
+++ test/GameObjectPhysics_test.cpp
@@ -32,15 +32,63 @@ TEST(move, newPositionCorrect_angle0)
 
     DerivedGameObject oldObj = obj;
 
-    EXPECT_EQ(obj.topLeft().x, oldObj.topLeft().x);
-    EXPECT_EQ(obj.topLeft().y, oldObj.topLeft().y);
+    auto timeInMS = 1000;
+
+    move(obj, timeInMS);
+
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, oldObj.topLeft().x + oldObj.velocity());
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, oldObj.topLeft().y);
+}
+
+TEST(move, newPositionCorrect_angle30)
+{
+    DerivedGameObject obj{Point{10.0, 10.0},    MaxPositionX{1000.0},
+                          MaxPositionY{1000.0}, Width{1.0},
+                          Height{1.0},          Velocity{2.0},
+                          Angle{30.0_deg}};
+
+    DerivedGameObject oldObj = obj;
 
     auto timeInMS = 1000;
 
     move(obj, timeInMS);
 
-    EXPECT_EQ(obj.topLeft().x, oldObj.topLeft().x + oldObj.velocity());
-    EXPECT_EQ(obj.topLeft().y, oldObj.topLeft().y);
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, 11.732050807568877);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, 11);
+}
+
+TEST(move, newPositionCorrect_angle45)
+{
+    DerivedGameObject obj{Point{10.0, 10.0},    MaxPositionX{1000.0},
+                          MaxPositionY{1000.0}, Width{1.0},
+                          Height{1.0},          Velocity{2.0},
+                          Angle{45.0_deg}};
+
+    DerivedGameObject oldObj = obj;
+
+    auto timeInMS = 1000;
+
+    move(obj, timeInMS);
+
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, 11.414213562373096);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, 11.414213562373096);
+}
+
+TEST(move, newPositionCorrect_angle60)
+{
+    DerivedGameObject obj{Point{10.0, 10.0},    MaxPositionX{1000.0},
+                          MaxPositionY{1000.0}, Width{1.0},
+                          Height{1.0},          Velocity{2.0},
+                          Angle{60.0_deg}};
+
+    DerivedGameObject oldObj = obj;
+
+    auto timeInMS = 1000;
+
+    move(obj, timeInMS);
+
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, 11);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, 11.732050807568877);
 }
 
 TEST(move, newPositionCorrect_angle90)
@@ -52,15 +100,46 @@ TEST(move, newPositionCorrect_angle90)
 
     DerivedGameObject oldObj = obj;
 
-    EXPECT_EQ(obj.topLeft().x, oldObj.topLeft().x);
-    EXPECT_EQ(obj.topLeft().y, oldObj.topLeft().y);
+    auto timeInMS = 1000;
+
+    move(obj, timeInMS);
+
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, oldObj.topLeft().x);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, oldObj.topLeft().y + oldObj.velocity());
+}
+
+TEST(move, newPositionCorrect_angle120)
+{
+    DerivedGameObject obj{Point{10.0, 10.0},    MaxPositionX{1000.0},
+                          MaxPositionY{1000.0}, Width{1.0},
+                          Height{1.0},          Velocity{2.0},
+                          Angle{120.0_deg}};
+
+    DerivedGameObject oldObj = obj;
 
     auto timeInMS = 1000;
 
     move(obj, timeInMS);
 
-    EXPECT_EQ(obj.topLeft().x, oldObj.topLeft().x);
-    EXPECT_EQ(obj.topLeft().y, oldObj.topLeft().y + oldObj.velocity());
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, 9);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, 11.732050807568877);
+}
+
+TEST(move, newPositionCorrect_angle135)
+{
+    DerivedGameObject obj{Point{10.0, 10.0},    MaxPositionX{1000.0},
+                          MaxPositionY{1000.0}, Width{1.0},
+                          Height{1.0},          Velocity{2.0},
+                          Angle{135.0_deg}};
+
+    DerivedGameObject oldObj = obj;
+
+    auto timeInMS = 1000;
+
+    move(obj, timeInMS);
+
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, 8.5857864376269042);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, 11.414213562373096);
 }
 
 TEST(move, newPositionCorrect_angle180)
@@ -72,15 +151,29 @@ TEST(move, newPositionCorrect_angle180)
 
     DerivedGameObject oldObj = obj;
 
-    EXPECT_EQ(obj.topLeft().x, oldObj.topLeft().x);
-    EXPECT_EQ(obj.topLeft().y, oldObj.topLeft().y);
+    auto timeInMS = 1000;
+
+    move(obj, timeInMS);
+
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, oldObj.topLeft().x - oldObj.velocity());
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, oldObj.topLeft().y);
+}
+
+TEST(move, newPositionCorrect_angle230)
+{
+    DerivedGameObject obj{Point{10.0, 10.0},    MaxPositionX{1000.0},
+                          MaxPositionY{1000.0}, Width{1.0},
+                          Height{1.0},          Velocity{2.0},
+                          Angle{230.0_deg}};
+
+    DerivedGameObject oldObj = obj;
 
     auto timeInMS = 1000;
 
     move(obj, timeInMS);
 
-    EXPECT_EQ(obj.topLeft().x, oldObj.topLeft().x - oldObj.velocity());
-    EXPECT_EQ(obj.topLeft().y, oldObj.topLeft().y);
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, 8.7144247806269206);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, 8);
 }
 
 TEST(move, newPositionCorrect_angle270)
@@ -92,13 +185,27 @@ TEST(move, newPositionCorrect_angle270)
 
     DerivedGameObject oldObj = obj;
 
-    EXPECT_EQ(obj.topLeft().x, oldObj.topLeft().x);
-    EXPECT_EQ(obj.topLeft().y, oldObj.topLeft().y);
+    auto timeInMS = 1000;
+
+    move(obj, timeInMS);
+
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, oldObj.topLeft().x);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, oldObj.topLeft().y - oldObj.velocity());
+}
+
+TEST(move, newPositionCorrect_angle315)
+{
+    DerivedGameObject obj{Point{10.0, 10.0},    MaxPositionX{1000.0},
+                          MaxPositionY{1000.0}, Width{1.0},
+                          Height{1.0},          Velocity{2.0},
+                          Angle{315.0_deg}};
+
+    DerivedGameObject oldObj = obj;
 
     auto timeInMS = 1000;
 
     move(obj, timeInMS);
 
-    EXPECT_EQ(obj.topLeft().x, oldObj.topLeft().x);
-    EXPECT_EQ(obj.topLeft().y, oldObj.topLeft().y - oldObj.velocity());
+    EXPECT_DOUBLE_EQ(obj.topLeft().x, 11.414213562373096);
+    EXPECT_DOUBLE_EQ(obj.topLeft().y, 8.58579);
 }
