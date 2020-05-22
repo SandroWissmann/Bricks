ae3abd1a9fed5fc91416eccde96b71d77905e570
sandro4912 <sandro.wissmann@gmail.com>
2020-05-22 13:55:59 +0200

    test: adds test for class Brick

---
+++ test/Brick_test.cpp
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+
+#include "../src/Brick.h"
+
+using namespace bricks;
+
+TEST(BrickTest, startHitpoints)
+{
+    Brick obj{Point{0,0}, Width{1}, Height{1}, 2};
+    EXPECT_EQ(obj.startHitpoints(), 2);
+}
+
+TEST(BrickTest, hitpoints)
+{
+    Brick obj{Point{0,0}, Width{1}, Height{1}, 2};
+    EXPECT_EQ(obj.hitpoints(), 2);
+}
+
+TEST(BrickTest, decreaseHitpoints)
+{
+    Brick obj{Point{0,0}, Width{1}, Height{1}, 2};
+    EXPECT_EQ(obj.hitpoints(), 2);
+    EXPECT_EQ(obj.startHitpoints(), 2);
+
+    obj.decreaseHitpoints();
+
+    EXPECT_EQ(obj.hitpoints(), 1);
+    EXPECT_EQ(obj.startHitpoints(), 2);
+}
+
+TEST(BrickTest, isDestroyed)
+{
+    Brick obj{Point{0,0}, Width{1}, Height{1}, 1};
+    EXPECT_FALSE(obj.isDestroyed());
+
+    obj.decreaseHitpoints();
+
+    EXPECT_TRUE(obj.isDestroyed());
+}
