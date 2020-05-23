544f13d918f6dedb878d0c9cfb18fbe64c32b51d
sandro4912 <sandro.wissmann@gmail.com>
2020-05-23 17:16:06 +0200

    feat: adds function to verify if string is convertible to int or double

---
+++ src/IsNumber.cpp
@@ -0,0 +1,22 @@
+#include <IsNumber.h>
+
+#include <sstream>
+
+namespace bricks{
+
+    template <typename T>
+    bool isNumber(const std::string& s) {
+    if (s.empty()) {
+        return false;
+    }
+
+    std::istringstream ist(s);
+    T n;
+    ist >> std::noskipws >> n;
+
+    return ist.eof() && !ist.fail();
+}
+
+template bool isNumber<int>(const std::string& s);
+template bool isNumber<double>(const std::string& s);
+}
---
+++ src/IsNumber.h
@@ -0,0 +1,13 @@
+#ifndef ISNUMBER_H
+#define ISNUMBER_H
+
+#include <string>
+
+namespace bricks {
+
+template <typename T>
+bool isNumber(const std::string& s);
+
+}  // namespace bricks
+
+#endif
