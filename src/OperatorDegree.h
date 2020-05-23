43acf88961b3c7190635a7a78511813e65f16506
sandro4912 <sandro.wissmann@gmail.com>
2020-05-23 10:45:22 +0200

    fix: makes PI constexpr

--- src/OperatorDegree.h
+++ src/OperatorDegree.h
@@ -1,15 +1,14 @@
 #ifndef OPERATORDEGREE_H
 #define OPERATORDEGREE_H
 
+#define _USE_MATH_DEFINES
 #include <cmath>
 
 namespace bricks {
 
-constexpr long double pi() { return std::atan(1) * 4; }
-
 constexpr long double operator"" _deg(long double deg)
 {
-    return deg * pi() / 180.0L;
+    return deg * M_PI / 180.0L;
 }
 } // namespace bricks
 #endif
