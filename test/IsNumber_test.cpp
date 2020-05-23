d89f49d7025f56b7f6d38622f4b66296d2b218c3
sandro4912 <sandro.wissmann@gmail.com>
2020-05-23 17:16:23 +0200

    test: adds unit tests

---
+++ test/IsNumber_test.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+#include "../src/IsNumber.h"
+
+using namespace bricks;
+
+TEST(IsNumberTest, int_expect_true)
+{
+    EXPECT_TRUE(isNumber<int>("10"));
+    EXPECT_TRUE(isNumber<int>("0"));
+    EXPECT_TRUE(isNumber<int>("-10"));
+}
+
+TEST(IsNumberTest, int_expect_false)
+{
+    EXPECT_FALSE(isNumber<int>("10N"));
+    EXPECT_FALSE(isNumber<int>("0.0"));
+    EXPECT_FALSE(isNumber<int>("-10.0"));
+}
+
+TEST(IsNumberTest, double_expect_true)
+{
+    EXPECT_TRUE(isNumber<double>("10.0"));
+    EXPECT_TRUE(isNumber<double>("0.0"));
+    EXPECT_TRUE(isNumber<double>("-10.0"));
+    EXPECT_TRUE(isNumber<double>("0"));
+    EXPECT_TRUE(isNumber<double>("-10"));
+}
+
+TEST(IsNumberTest, double_expect_false)
+{
+    EXPECT_FALSE(isNumber<double>("10N"));
+}
