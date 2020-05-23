94c01b2611884b18ff91ba193aa3777681a8451b
sandro4912 <sandro.wissmann@gmail.com>
2020-05-23 17:46:39 +0200

    test: test istream operator of Point

---
+++ test/Point_test.cpp
@@ -0,0 +1,86 @@
+#include "gtest/gtest.h"
+
+#include "../src/Point.h"
+
+#include <iostream>
+#include <sstream>
+
+using namespace bricks;
+
+TEST(PointTest, operatorIsWorks1)
+{
+    std::string test {
+        "X 3.5 Y 5.2"
+    };
+
+    std::istringstream ist{test};
+
+    Point p;
+    ist >> p;
+
+    EXPECT_FALSE(ist.fail());
+
+    EXPECT_EQ(p.x, 3.5);
+    EXPECT_EQ(p.y, 5.2);
+}
+
+TEST(PointTest, operatorIsWorks2)
+{
+    std::string test {
+        "X 3 Y 5"
+    };
+
+    std::istringstream ist{test};
+
+    Point p;
+    ist >> p;
+
+    EXPECT_FALSE(ist.fail());
+
+    EXPECT_EQ(p.x, 3.0);
+    EXPECT_EQ(p.y, 5.0);
+}
+
+
+TEST(PointTest, operatorIsFails1)
+{
+    std::string test {
+        " 3.5 Y 5.2"
+    };
+
+    std::istringstream ist{test};
+
+    Point p;
+    ist >> p;
+
+    EXPECT_TRUE(ist.fail());
+}
+
+
+TEST(PointTest, operatorIsFails2)
+{
+    std::string test {
+        "X 3.5  5.2"
+    };
+
+    std::istringstream ist{test};
+
+    Point p;
+    ist >> p;
+
+    EXPECT_TRUE(ist.fail());
+}
+
+TEST(PointTest, operatorIsFails3)
+{
+    std::string test {
+        "Y 3.5 X 5.2"
+    };
+
+    std::istringstream ist{test};
+
+    Point p;
+    ist >> p;
+
+    EXPECT_TRUE(ist.fail());
+}
