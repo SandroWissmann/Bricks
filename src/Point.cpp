8cb6c4b4a405247d016e06ed11127e7daea69e1d
sandro4912 <sandro.wissmann@gmail.com>
2020-05-23 17:46:22 +0200

    feat: adds istream operator to point

---
+++ src/Point.cpp
@@ -0,0 +1,42 @@
+#include "Point.h"
+
+#include "IsNumber.h"
+
+#include <iostream>
+
+namespace bricks{
+
+    std::istream& operator>>(std::istream& is, Point& obj)
+    {
+        std::string s;
+        is >> s;
+        if(s != "X") {
+            is.setstate(std::ios_base::failbit);
+            return is;
+        }
+
+        is >> s;
+        if(!isNumber<double>(s))  {
+            is.setstate(std::ios_base::failbit);
+            return is;        
+        }
+        auto x = std::stod(s);
+
+        is >> s;
+        if(s != "Y") {
+            is.setstate(std::ios_base::failbit);
+            return is;
+        }
+        
+        is >> s;
+        if(!isNumber<double>(s))  {
+            is.setstate(std::ios_base::failbit);
+            return is;        
+        }
+        auto y = std::stod(s);
+
+        obj.x = x;
+        obj.y = y;
+        return is;
+    }
+}
--- src/point.h
+++ src/point.h
@@ -1,11 +1,16 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include <iosfwd>
+
 namespace bricks {
 
 struct Point {
     double x{0.0};
     double y{0.0};
 };
+
+std::istream& operator>>(std::istream& is, Point& obj);
+
 } // namespace bricks
 #endif
