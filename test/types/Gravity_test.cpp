7e309eadd77cf66f6a73dd49814f69dcb26c43fc
sandro4912 <sandro.wissmann@gmail.com>
2020-05-24 10:16:57 +0200

    test: adds constructor tests

---
+++ test/types/Gravity_test.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+#include "../../src/types/Gravity.h"
+
+#include <iostream>
+#include <sstream>
+
+using namespace bricks::types;
+
+TEST(GravityTest, defaultConstructor)
+{
+    Gravity g;
+    EXPECT_EQ(g(), 0.0);
+}
+
+TEST(GravityTest, Constructor)
+{
+    Gravity g{3.5};
+    EXPECT_EQ(g(), 3.5);
+}
