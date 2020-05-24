30f91afe2159c4142714b84be2489ebd20cf8006
sandro4912 <sandro.wissmann@gmail.com>
2020-05-24 10:11:24 +0200

    test: adds tests

---
+++ test/types/Hitpoints_test.cpp
@@ -0,0 +1,70 @@
+#include "gtest/gtest.h"
+
+#include "../../src/types/Hitpoints.h"
+
+#include <iostream>
+#include <sstream>
+
+using namespace bricks::types;
+
+TEST(HitpointsTest, defaultConstructor)
+{
+    Hitpoints hp;
+    EXPECT_EQ(hp(), 0);
+}
+
+TEST(HitpointsTest, Constructor)
+{
+    Hitpoints hp{3};
+    EXPECT_EQ(hp(), 3);
+}
+
+TEST(HitpointsTest, operatorIsWorks1)
+{
+    std::string test{"HP 3"};
+
+    std::istringstream ist{test};
+
+    Hitpoints hp;
+    ist >> hp;
+
+    EXPECT_FALSE(ist.fail());
+
+    EXPECT_EQ(hp(), 3);
+}
+
+TEST(HitpointsTest, operatorIsFails1)
+{
+    std::string test{" 3"};
+
+    std::istringstream ist{test};
+
+    Hitpoints hp;
+    ist >> hp;
+
+    EXPECT_TRUE(ist.fail());
+}
+
+TEST(HitpointsTest, operatorIsFails2)
+{
+    std::string test{"HP 3.5"};
+
+    std::istringstream ist{test};
+
+    Hitpoints hp;
+    ist >> hp;
+
+    EXPECT_TRUE(ist.fail());
+}
+
+TEST(HitpointsTest, operatorIsFails3)
+{
+    std::string test{"H 3"};
+
+    std::istringstream ist{test};
+
+    Hitpoints hp;
+    ist >> hp;
+
+    EXPECT_TRUE(ist.fail());
+}
