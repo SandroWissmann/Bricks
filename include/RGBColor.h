9d1e4e9c8c9138b800a7e6fd146b9b1af07c221b
sandro4912 <sandro.wissmann@gmail.com>
2020-05-25 17:50:39 +0200

    feat: adds class to represent RGBColor

---
+++ include/RGBColor.h
@@ -0,0 +1,40 @@
+#include <cassert>
+
+namespace bricks {
+    class RGBColor {
+    public:
+        constexpr RGBColor(int r, int g, int b, int a = 0xFF)
+            :mR{r}, mG{g}, mB{b}, mA{a}
+        {
+            assert(mR >= 0 && mR <= 0xFF);
+            assert(mG >= 0 && mG <= 0xFF);
+            assert(mB >= 0 && mB <= 0xFF);
+            assert(mA >= 0 && mA <= 0xFF);
+        }
+
+        constexpr int r() const
+        {
+            return mR;
+        }
+
+        constexpr int g() const
+        {
+            return mG;
+        }
+
+        constexpr int b() const
+        {
+            return mB;
+        }
+        
+        constexpr int a() const
+        {
+            return mA;
+        }
+    private:
+        const int mR;
+        const int mG;
+        const int mB;
+        const int mA;
+    };
+}
--- test/RGBColor_test.cpp
+++ test/RGBColor_test.cpp
@@ -2,32 +2,13 @@
 
 #include "../include/RGBColor.h"
 
-// using namespace bricks;
-// using namespace bricks::types;
+using namespace bricks;
 
-// class PlatformTest : public ::testing::Test {
-// protected:
-//     Point point{10.0, 10.0};
-//     Length length{1.0};
-//     Width width{1.0};
-//     MaxPositionX maxPositionX{1000.0};
-//     MaxPositionY maxPositionY{1000.0};
-//     Velocity velocity{2.0};
-
-//     Platform makePlatform(const Velocity &velocity)
-//     {
-//         return Platform{point,        length,       width,
-//                         maxPositionX, maxPositionY, velocity};
-//     }
-// };
-
-// TEST_F(PlatformTest, move_positive)
-// {
-//     auto obj = makePlatform(Velocity{2.0});
-//     auto oldObj = obj;
-//     auto timeInMS = 1000;
-//     obj.move(timeInMS);
-
-//     EXPECT_DOUBLE_EQ(obj.topLeft().x, oldObj.topLeft().x + oldObj.velocity());
-//     EXPECT_DOUBLE_EQ(obj.topLeft().y, oldObj.topLeft().y);
-// }
+TEST(RGBColorTest, Constructor)
+{
+    RGBColor rgbColor{0x20, 0x10, 0x30, 0x40};
+    EXPECT_EQ(rgbColor.r(), 0x20);
+    EXPECT_EQ(rgbColor.g(), 0x10);
+    EXPECT_EQ(rgbColor.b(), 0x30);
+    EXPECT_EQ(rgbColor.a(), 0x40);
+}
