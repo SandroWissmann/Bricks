fcc5b1bb7a53364db7acc9b7c30d09ee2a492ba7
sandro4912 <sandro.wissmann@gmail.com>
2020-05-26 16:51:12 +0200

    feat: Adds class to represent width of the game area

---
+++ include/types/GridWidth.h
@@ -0,0 +1,22 @@
+#ifndef TYPES_GRIDWIDTH_H
+#define TYPES_GRIDWIDTH_H
+
+#include <iosfwd>
+
+namespace bricks::types {
+
+class GridWidth {
+public:
+    GridWidth() = default;
+    GridWidth(int value);
+
+    int operator()() const;
+
+private:
+    int mValue{0};
+};
+
+std::istream &operator>>(std::istream &is, GridWidth &obj);
+
+} // namespace bricks::types
+#endif
---
+++ src/types/GridWidth.cpp
@@ -0,0 +1,38 @@
+#include "GridWidth.h"
+
+#include "../IsNumber.h"
+
+#include <iostream>
+
+namespace bricks::types {
+
+GridWidth::GridWidth(int value) : mValue{value}
+{
+}
+
+int GridWidth::operator()() const
+{
+    return mValue;
+}
+
+std::istream &operator>>(std::istream &is, GridWidth &obj)
+{
+    std::string s;
+    is >> s;
+    if (s != "W") {
+        is.setstate(std::ios_base::failbit);
+        return is;
+    }
+
+    is >> s;
+    if (!isNumber<int>(s)) {
+        is.setstate(std::ios_base::failbit);
+        return is;
+    }
+    auto w = std::stoi(s);
+    obj = std::move(GridWidth{w});
+
+    return is;
+}
+
+} // namespace bricks::types
