ee771a8b9f839502332f3104193c5e6b05551cb0
sandro4912 <sandro.wissmann@gmail.com>
2020-05-27 15:02:44 +0200

    test: dummy tests loading from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 #include "Renderer.h"
 
-#include "Platform.h"
-#include "Ball.h"
 #include "Level.h"
-#include "Wall.h"
 
 #include "OperatorDegree.h"
 
 #include <thread>
+#include <string>
 
 using namespace bricks;
 
@@ -19,48 +17,11 @@ int main()
     constexpr std::size_t boardHeight{18};
     Renderer renderer{screenWidth, screenHeight, boardWith, boardHeight};
 
-    bricks::Platform platform{
-        types::Point{ (boardWith / 2.0) - 2.0 , boardHeight -1.0},
-        types::Length{4.0},
-        types::Width{0.5},
-        types::MaxPositionX{boardWith},
-        types::MaxPositionY{boardHeight},
-        types::Velocity{18.0}
-    };
-
-    bricks::Ball ball{
-        types::Point{boardWith / 2.0, boardHeight -2.0},
-        types::Length{0.75},
-        types::Width{0.75},
-        types::MaxPositionX{boardWith},
-        types::MaxPositionY{boardHeight},
-        types::Velocity{16.0},
-        types::Angle{135.0_deg},
-        types::Gravity{}
-    };
-
-    std::vector<Wall> walls{
-        Wall{
-            types::Point{0.0, 0.0},
-            types::Length{1.0},
-            types::Width{boardHeight}
-        },
-        Wall{
-            types::Point{boardWith - 1.0},
-            types::Length{1.0},
-            types::Width{boardHeight}
-        },
-        Wall{
-            types::Point{1, 0},
-            types::Length{ boardWith - 2.0 * 1.0},
-            types::Width{1.0}
-        }
-    };
-
-    bricks::Level level;
+    auto level = bricks::readFromFile(
+        "/mnt/Programmierung/Cpp/Udacity/Project/Bricks/level/5.lvl");
 
     while(true) {
-        renderer.render(ball, platform, walls, level);
+        renderer.render(level);
 
         std::this_thread::sleep_for(std::chrono::milliseconds{100});
     }
