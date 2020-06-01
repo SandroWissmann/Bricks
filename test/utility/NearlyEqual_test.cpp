ae9cd39d8dbf2a860b2b1bbea0ea34de81fd32e4
sandro4912 <sandro.wissmann@gmail.com>
2020-06-01 10:58:41 +0200

    test: adds basic test

--- test/utility/NearlyEqual_test.cpp
+++ test/utility/NearlyEqual_test.cpp
@@ -2,8 +2,10 @@
 
 #include "../../include/utility/NearlyEqual.h"
 
+#include <limits>
+
 using namespace bricks::utility;
 
-// TEST(NearlyEqual, ValuesAreEqual_1) {
-//     EXPECT_TRUE(bricks::nearlyEqual<double>(1.0, 1.0));
-// }
+TEST(NearlyEqual, ValuesAreEqual_1) {
+    EXPECT_TRUE(nearlyEqual<double>(1.0, 1.0));
+}
