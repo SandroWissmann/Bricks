cfbbbf0757b81d8e6513d7ea8cb1a1d37beeb537
sandro4912 <sandro.wissmann@gmail.com>
2020-06-01 10:36:08 +0200

    fix: adds missing catch of exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include "Game.h"
 
+#include <iostream>
+
 int main()
 {
-    constexpr std::size_t screenWidth{780};
-    constexpr std::size_t screenHeight{540};
+    try{
+        constexpr std::size_t screenWidth{780};
+        constexpr std::size_t screenHeight{540};
 
-    bricks::Game game{screenWidth, screenHeight};
-    game.run();
+        bricks::Game game{screenWidth, screenHeight};
+        game.run();
+    }
+    catch (const std::out_of_range& e) {
+        std::cerr << e.what() << "\n";
+        std::cin.get();
+    }
+    catch (const std::runtime_error& e) {
+        std::cerr << e.what() << "\n";
+        std::cin.get();
+    }
+    catch (const std::invalid_argument& e) {
+        std::cerr << e.what() << "\n";
+        std::cin.get();
+    }
+    catch (...) {
+        std::cerr << "unknown error " << "\n";
+        std::cin.get();
+    }
 }
