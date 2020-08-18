d5bf0ef62df4c2b088892285ff6bda050a4c5900
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-18 16:33:31 +0200

    fix: adds missing test for move and fixxes parameters

--- test/game_objects/Ball_test.cpp
+++ test/game_objects/Ball_test.cpp
@@ -69,61 +69,60 @@ protected:
 
 TEST_P(BallMoveMultipleParametersTests, CheckIfMovesCorrectly)
 {
-    // bool active = std::get<0>(GetParam());
-    // Velocity velocity{std::get<1>(GetParam())};
-    // Angle angle{std::get<2>(GetParam())};
-    // Gravity gravity{std::get<3>(GetParam())};
-    // Point endPoint = std::get<4>(GetParam());
+    bool active = std::get<0>(GetParam());
+    Velocity velocity{std::get<1>(GetParam())};
+    Angle angle{std::get<2>(GetParam())};
+    Gravity gravity{std::get<3>(GetParam())};
+    Point endPoint = std::get<4>(GetParam());
 
-    // Ball obj{Point{0.0, 0.0}, Width{3.1}, Height{4.2},
-    //          velocity,        angle,      gravity};
-    // if (active) {
-    //     obj.activate();
-    // }
-    // auto timeInMS = 1000;
-    // obj.move(timeInMS);
+    Ball obj{Point{0.0, 0.0}, Width{3.1}, Height{4.2},
+             velocity,        angle,      gravity};
+    if (active) {
+        obj.activate();
+    }
+    auto timeInMS = 1000;
+    obj.move(timeInMS);
 
-    // EXPECT_DOUBLE_EQ(obj.topLeft().x, endPoint.x);
-    // EXPECT_DOUBLE_EQ(obj.topLeft().y, endPoint.y);
+    EXPECT_NEAR(obj.topLeft().x, endPoint.x, 0.000000001);
+    EXPECT_NEAR(obj.topLeft().y, endPoint.y, 0.000000001);
 }
 
 INSTANTIATE_TEST_CASE_P(
     BallMoveTests, BallMoveMultipleParametersTests,
     ::testing::Values(
         // not active
-        std::make_tuple(false, 2.0, 0.0, 0.0, Point{0.0, 0.0}),
+        std::make_tuple(false, 2.0, 0.0_deg, 0.0, Point{0.0, 0.0}),
         // only gravity
-        std::make_tuple(true, 0.0, 0.0, 2.0, Point{0.0, 2.0}),
+        std::make_tuple(true, 0.0, 0.0_deg, 2.0, Point{0.0, 2.0}),
         // 0 degrees
-        std::make_tuple(true, 2.0, 0.0, 0.0, Point{2.0, 0.0}),
+        std::make_tuple(true, 2.0, 0.0_deg, 0.0, Point{2.0, 0.0}),
         // 30 degrees
-        std::make_tuple(true, 2.0, 30.0, 0.0, Point{1.732050807568877, 1}),
+        std::make_tuple(true, 2.0, 30.0_deg, 0.0, Point{1.732050807568877, 1}),
         // 45 degrees
-        std::make_tuple(true, 2.0, 45.0, 0.0,
+        std::make_tuple(true, 2.0, 45.0_deg, 0.0,
                         Point{1.414213562373096, 1.414213562373096}),
         // 60 degrees
-        std::make_tuple(true, 2.0, 60.0, 0.0, Point{1.0, 1.732050807568877}),
+        std::make_tuple(true, 2.0, 60.0_deg, 0.0,
+                        Point{1.0, 1.732050807568877}),
         // 90 degrees
-        std::make_tuple(true, 2.0, 90.0, 0.0, Point{0.0, 2.0}),
+        std::make_tuple(true, 2.0, 90.0_deg, 0.0, Point{0.0, 2.0}),
         // 90 degrees + gravity
-        std::make_tuple(true, 2.0, 90.0, 2.0, Point{0.0, 4.0}),
+        std::make_tuple(true, 2.0, 90.0_deg, 2.0, Point{0.0, 4.0}),
         // 120 degrees
-        std::make_tuple(true, 2.0, 12.0, 0.0, Point{-1, 1.732050807568877}),
+        std::make_tuple(true, 2.0, 120.0_deg, 0.0,
+                        Point{-1, 1.732050807568877}),
         // 135 degrees
-        std::make_tuple(true, 2.0, 135.0, 0.0,
+        std::make_tuple(true, 2.0, 135.0_deg, 0.0,
                         Point{-1.41421356237, 1.414213562373096}),
         // 180 degrees
-        std::make_tuple(true, 2.0, 180.0, 0.0, Point{-2.0, 0.0}),
+        std::make_tuple(true, 2.0, 180.0_deg, 0.0, Point{-2.0, 0.0}),
         // 230 degrees
-        std::make_tuple(true, 2.0, 230.0, 0.0,
+        std::make_tuple(true, 2.0, 230.0_deg, 0.0,
                         Point{-1.28557521937, -1.53208888624}),
         // 270 degrees
-        std::make_tuple(true, 2.0, 270.0, 0.0, Point{0.0, -2.0}),
+        std::make_tuple(true, 2.0, 270.0_deg, 0.0, Point{0.0, -2.0}),
         // 270 degrees + gravity
-        std::make_tuple(true, 2.0, 270.0, 2.0, Point{0.0, -2.0})));
-
-// INSTANTIATE_TEST_CASE_P(BallMoveTests, BallMoveMultipleParametersTests,
-//                         ::testing::Values(std::make_tuple(
-//                             false, 2.0, 0.0, 0.0,
-//                             bricks::types::Point{0.0, 0.0}) // not active
-//                                           ));
+        std::make_tuple(true, 2.0, 270.0_deg, 2.0, Point{0.0, 0.0}),
+        // 315 degress
+        std::make_tuple(true, 2.0, 315.0_deg, 0.0,
+                        Point{1.414213562373096, -1.414213562373096})));
